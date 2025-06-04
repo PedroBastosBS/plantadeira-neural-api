025927e11f99cd21e49edd6df3f76b993729948b
pedro.silva <pedrobastos750@gmail.com>
2025-06-04 14:13:29 -0400

    feat: ajustando entidade removendo coisas descessarias

--- domain/Entities/chat_message.go
+++ domain/Entities/chat_message.go
@@ -3,12 +3,11 @@ package Entities
 import "time"
 
 type ChatMessage struct {
-	ID        string    `json:"id"`
-	SessionID string    `json:"session_id"`
-	UserID    string    `json:"user_id"`
-	Role      string    `json:"role"`
-	Content   string    `json:"content"`
-	CreatedAt time.Time `json:"created_at"`
+	ID             string    `json:"id"`
+	UserID         string    `json:"user_id"`
+	ConversationID string    `json:"conversation_id"`
+	Content        string    `json:"content"`
+	CreatedAt      time.Time `json:"created_at"`
 }
 
 func NewChatMessage() *ChatMessage {
